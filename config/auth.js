2771083c2f1c66e0fb61d577796235d9a3c0e543
ducanh25121998 <65638502+ducanh25121998@users.noreply.github.com>
2020-12-03 00:35:31 +0700

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'        : '802058360591687', // your App ID
         'clientSecret'    : '72132375a001e3b81ab8dd839ecc0cdb', // your App Secret
-        'callbackURL'     : 'http://timvieclamthem.cleverapps.io/auth/facebook/callback',
+        'callbackURL'     : 'https://timvieclamthem.cleverapps.io/auth/facebook/callback',
         'profileURL': '     https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields'   : ['id', 'email', 'name'] // For requesting permissions from Facebook API
 
@@ -14,7 +14,7 @@ module.exports = {
     'googleAuth' : {
         'clientID'         : '163618400101-kgebgmhbdhv107q84c6foupkhpg83p63.apps.googleusercontent.com',
         'clientSecret'     : 'j6lpcyvYLwLgmLpXy6hVk8Yz',
-        'callbackURL'      : 'http://timvieclamthem.cleverapps.io/auth/google/callback'
+        'callbackURL'      : 'https://timvieclamthem.cleverapps.io/auth/google/callback'
     }
 
 };
